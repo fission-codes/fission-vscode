0f026660f7257d628b03eaea3e3aa1e2c7e38e79
Brian Ginsburg <gins@brianginsburg.com>
2021-04-16 15:49:17 -0700

    Upgrade to Fission CLI 2.10.2
    Upgrade all commands to match the Fission CLI Optimus Prime release. All
    commands now run from the root directory of a project and some helper functions
    were no longer needed.
    Commands focus to the terminal when user interaction is expected, otherwise
    they preserve focus in the current window.
    Updated README to match new commands and CLI installation instructions.
    Switched linter to expect spaces instead of tabs.

--- src/extension.ts
+++ src/extension.ts
@@ -1,74 +1,46 @@
-'use strict';
-
 import * as vscode from 'vscode';
 
 export function activate(context: vscode.ExtensionContext) {
-  context.subscriptions.push(vscode.commands.registerCommand('fissionCommand.register', sendAndFocus("fission register")));
-  context.subscriptions.push(vscode.commands.registerCommand('fissionCommand.login', sendAndFocus("fission login")));
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fissionCommand.setup', sendCommand("fission setup", false))
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fissionCommand.whoami', sendCommand("fission whoami", true))
+  );
 
-  context.subscriptions.push(vscode.commands.registerCommand('fissionCommand.up', sendAndFocus("fission up")));
-  context.subscriptions.push(vscode.commands.registerCommand('fissionCommand.upFile', runCommandOnFile("fission up")));
-  context.subscriptions.push(vscode.commands.registerCommand('fissionCommand.upFolder', runCommandOnFolder("fission up")));
-
-  context.subscriptions.push(vscode.commands.registerCommand('fissionCommand.watch', sendAndFocus("fission watch")));
-  context.subscriptions.push(vscode.commands.registerCommand('fissionCommand.watchFile', runCommandOnFile("fission watch")));
-  context.subscriptions.push(vscode.commands.registerCommand('fissionCommand.watchFolder', runCommandOnFolder("fission watch")));
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fissionCommand.appRegister', sendCommand("fission app register", false))
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fissionCommand.appInfo', sendCommand("fission app info", true))
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fissionCommand.appPublish', sendCommand("fission app publish", true))
+  );
+  context.subscriptions.push(
+    vscode.commands.registerCommand('fissionCommand.appPublishWatch', sendCommand("fission app publish --watch", true))
+  );
 }
 
 /**
- * Runs a given command with the current file path as an argument
+ * Open a terminal and send it a command
  */
-const runCommandOnFile = (command: string) => () => {
-  const uri = getOpenFileURI();
-  if (uri) {
-    sendAndFocus(`${command} ${uri.fsPath}`)()
-  }
-}
-
-/**
- * Runs a given command with the current folder path as an argument
- */
-const runCommandOnFolder = (command: string) => () => {
-  const uri = getOpenFileURI();
-  if (uri) {
-    const folderPath = uri.fsPath.split('/').slice(1, -1).join('/');
-    sendAndFocus(`${command} /${folderPath}`)()
-  }
-}
-
-/**
- * Gets the current URI of the open text editor
- */
-const getOpenFileURI = () => {
-  const activeTextEditor = vscode.window.activeTextEditor;
-  if (activeTextEditor) {
-    return activeTextEditor.document.uri;
-  } else {
-    vscode.window.showErrorMessage('No editor active');
-    return undefined;
-  }
-}
-
-/**
- * Opens a terminal and sends it a command
- */
-const sendAndFocus = (command: string) => () => {
-  const terminal = quickGetTerminal()
+const sendCommand = (command: string, preserveFocus: boolean) => () => {
+  const terminal = getTerminal();
   if (terminal) {
-    terminal.show(true);
+    terminal.show(preserveFocus);
     terminal.sendText(command);
-    vscode.window.showInformationMessage(`Successfully ran ${command}`);
   } else {
-    vscode.window.showErrorMessage('Uh oh! No active terminals found...');
+    vscode.window.showErrorMessage('No active terminals found.');
   }
-}
+};
 
 /**
- * Quickly get an existing terminal or open a new one in the case one doesn't exist
+ * Get an existing terminal or open a new one if none are open
  */
-function quickGetTerminal(): vscode.Terminal | undefined {
+function getTerminal(): vscode.Terminal | undefined {
   const terminals = <vscode.Terminal[]>(<any>vscode.window).terminals;
   return terminals.length > 0
     ? terminals[0]
-    : vscode.window.createTerminal("Fission");;
+    : vscode.window.createTerminal("Fission");
 }
